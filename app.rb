2691402d4593c78bfb44446fdf229b99e8ca55df
Irene Scott <irenejinscott@gmail.com>
2020-06-22 13:24:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
     #hash
     session["socks"] = "socks"
     @session = session
-    params[:item] = session
+    params[:item] = @session
 
   end
     #key should be item
