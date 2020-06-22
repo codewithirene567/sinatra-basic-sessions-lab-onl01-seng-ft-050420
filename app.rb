9bc0f0b0bdfe0a4989fc8ee6cca053cede572a3b
Irene Scott <irenejinscott@gmail.com>
2020-06-22 13:14:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   post '/checkout' do
     #take in params from the form and add it to the session
     #hash
-    params[:item]
+    @variable = params[:item]
     #key should be item
 #store the session hash in an instance variable that you can
 #access in the views
